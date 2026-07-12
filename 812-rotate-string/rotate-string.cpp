8f28f2612bc50d95342b07be6d66e1dd98a64ec5
Bhuvi Jain <jainbhuvi7@gmail.com>
2026-07-12 22:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (93.38%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()){
+        // If the lengths are different, s can never be rotated into goal
+        if (s.length() != goal.length()) {
             return false;
         }
-        string doubled = s+s;//
+        
+        // Concatenating s with itself contains all possible rotations of s
+        string doubled = s + s;
+        
+        // Check if goal is a substring of the doubled string
         return doubled.find(goal) != string::npos;
     }
 };
